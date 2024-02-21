334666e0694b8ebdf6a826957b68e3074ccb9ddc
zulemaromero <158268485+zulemaromero@users.noreply.github.com>
2024-02-21 10:25:15 +0100

    Update dauss.py

--- dauss.py
+++ dauss.py
@@ -1,3 +1,31 @@
+
+import random
+
+print(random.randint(1, 6))
+
+def dau_6():
+    import random
+    print(random.randint(1, 6))
+
+def daus_6():
+    tirades = int(input("Quants daus vols llençar?"))
+    for i in range(tirades):
+        tirada = random.randint(1,6)
+        print("Tirada",i+1,":",tirada)
+
+
+def dau_x():
+    cares = int(input("Quantes cares te el dau?"))
+    print(random.randint(1,cares))
+
+def daus_x():
+    cares = int(input("Quantes cares te el dau?"))
+    num_daus = int(input("Quants daus?"))
+    for i in range(num_daus):
+        print(random.randint(1,cares))
+
+###########################
+
 import Daus
 
 while True:
