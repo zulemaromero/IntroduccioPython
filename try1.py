3f8d5dad37607b89cf25a828dd07de9aef644f6e
zulemaromero <158268485+zulemaromero@users.noreply.github.com>
2024-02-26 11:21:52 +0100

    Update try1.py

--- try1.py
+++ try1.py
@@ -1,6 +1,6 @@
-numero1 = float(input("Digues un numero: "))
-numero2 = float(input("Digues un altre numero: "))
 try:
+    numero1 = float(input("Digues un numero: "))
+    numero2 = float(input("Digues un altre numero: "))
     resultat = numero1 / numero2
     print(resultat)
 except:
