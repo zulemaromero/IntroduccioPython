8c77122f8dac33d55737aa1147a2de4898c7b4fa
zulemaromero <158268485+zulemaromero@users.noreply.github.com>
2024-03-04 11:50:01 +0100

    Update w-s.py

--- pygame/w-s.py
+++ pygame/w-s.py
@@ -1,27 +1,36 @@
-moviment_pantalla = input("""Per anar amunt presiona "w"
-Per anar avall presiona "s" : """)
-alçada = 21
-posicio = 0
+vidas = int(input("Quantes vides tens? "))
+ample_caixa = 20
+alt_caixa = 10
+posicio_asterisc_x = 0
+posicio_asterisc_y = 0
+
 while True:
-    moviment_pantalla = input(":")
-    if moviment_pantalla == "w":
-        posicio = posicio - 1
-        if posicio < 0:
-            posicio = 0
-    elif moviment_pantalla == "s":
-        posicio = posicio + 1
-        if posicio >= alçada:
-            posicio = alçada - 1
-    if moviment_pantalla == "a":
-        print("Tecla erronea")
-    if moviment_pantalla == "d":
-        print("Tecla erronea")
-    text = ""
-    for i in range(alçada):
-        if i == posicio:
-            text += "*"
-        else:
-            text += "."
-        text += "\n"
-    print(text)
-    print(posicio)
+    print("-" * (ample_caixa + 2))
+
+    for i in range(alt_caixa):
+        print('|', end='')
+        for j in range(ample_caixa):
+            if i == posicio_asterisc_y and j == posicio_asterisc_x:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print('|')
+
+    print("-" * (ample_caixa + 2))
+
+    tecla = input("Clica 'a' per moure a l'esquerra, 'd' per moure a la dreta, 'w' per moure cap amunt, 's' per moure cap avall i 'k' per perdre una vida: ")
+
+    if tecla == 'a' and posicio_asterisc_x > 0:
+        posicio_asterisc_x -= 1
+    elif tecla == 'd' and posicio_asterisc_x < ample_caixa - 1:
+        posicio_asterisc_x += 1
+    elif tecla == 'w' and posicio_asterisc_y > 0:
+        posicio_asterisc_y -= 1
+    elif tecla == 's' and posicio_asterisc_y < alt_caixa - 1:
+        posicio_asterisc_y += 1
+    if tecla == 'k':
+        vidas -= 1
+        print("Has perdut una vids")
+    if vidas == 0:
+        print('Game Over!')
+        break
