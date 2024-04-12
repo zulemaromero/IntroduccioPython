572a141f1488f15fafeeefdd3b8065f6fff5e5a7
zulemaromero <158268485+zulemaromero@users.noreply.github.com>
2024-04-12 09:40:43 +0200

    Update PantallaInici.py

--- pygame/PantallaInici.py
+++ pygame/PantallaInici.py
@@ -1,72 +1,36 @@
-import time
-
 import pygame
 
 AMPLADA = 320
 ALTURA = 200
-BACKGROUND_IMAGE = 'assets/GameOn.png'
-LOGO_IMAGE = "assets/Enemic2.png"
+BACKGROUND_IMAGE = 'assets/galactic.png'
 
 pygame.init()
 pantalla = pygame.display.set_mode((AMPLADA, ALTURA))
 pygame.display.set_caption("Galactic Battle")
-logo = pygame.image.load(LOGO_IMAGE)
-pygame.display.set_icon(logo)
+background = pygame.image.load(BACKGROUND_IMAGE).convert()
 
-def imprimir_pantalla_fons(image):
-    # Imprimeixo imatge de fons:
-    background = pygame.image.load(image).convert()
-    pantalla.blit(background, (0, 0))
-def animacio_inici():
-    imprimir_pantalla_fons(BACKGROUND_IMAGE)
-    pygame.display.update()
-    # time.sleep(3)
-    # FONT I TEXT de tamany 64
-    font = pygame.font.SysFont(None, 35)
-    for i in range(120):
-        time.sleep(0.01)
-        imprimir_pantalla_fons(BACKGROUND_IMAGE)
-        img = font.render("Enjoy!", True, (255, 255, 255))
-        pantalla.blit(img, (20, 200-i))
-        pygame.display.update()
-    time.sleep(1)
+# CREAR LA SUPERFÍCIE TRANSPARENT I EL RECTANGLE SOBRE ELLA:
+seccio_transparent = pygame.Surface((155,200),pygame.SRCALPHA)
+pygame.draw.rect(seccio_transparent,(47,47,255,100),(0,0,240,100))
 
-
-def imprimir_menu():
-    imprimir_pantalla_fons(BACKGROUND_IMAGE)
-    # CREAR LA SUPERFÍCIE TRANSPARENT I EL RECTANGLE SOBRE ELLA:
-    seccio_transparent = pygame.Surface((240, 120), pygame.SRCALPHA)
-    pygame.draw.rect(seccio_transparent, (0, 0, 0, 100), (0, 0, 240, 120))
-    # DIBUIXAR LA SUPERFÍCIE TRANSPARENT A LA FINESTRA
-    pantalla.blit(seccio_transparent, (40, 40))
-    # FONT I TEXT de tamany 64
-    font = pygame.font.SysFont(None, 36)
-    img = font.render("1.- Crèdits", True, (255, 255, 255))
-    img2 = font.render("2.- Jugar", True, (255, 255, 255))
-    img3 = font.render("3.- Sortir", True, (255, 255, 255))
-    # dibuixem el text
-    pantalla.blit(img, (70, 50))
-    pantalla.blit(img2, (70, 90))
-    pantalla.blit(img3, (70, 130))
-    pygame.display.update()
-
-
-
-def musica_fons(musica):
-    ambient_music = pygame.mixer.Sound(musica)
-    music_chanel = pygame.mixer.Channel(0)
-    ambient_music.play()
-    while True:
-        ambient_music.set_volume(0.3)
-        time.sleep(0)
-        break
-
-musica_fons('assets/MusicaArcade.mp3')
-animacio_inici()
-imprimir_menu()
+#FONT I TEXT de tamany 64
+font = pygame.font.SysFont(None,32)
+img = font.render("1.- Crèdits", True, (255,255,255))
+img2 = font.render("2.- Jugar", True, (255,255,255))
+img3= font.render("3.- Sortir", True, (255,255,255))
 
 while True:
-    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    # Imprimeixo imatge de fons:
+    pantalla.blit(background, (0,0))
+    # DIBUIXAR LA SUPERFÍCIE TRANSPARENT A LA FINESTRA
+    pantalla.blit(seccio_transparent, (80, 50))
+    # dibuixem el text
+    pantalla.blit(img, (100, 60))
+    pantalla.blit(img2, (100, 90))
+    pantalla.blit(img3, (100, 120))
+
+    pygame.display.update()
